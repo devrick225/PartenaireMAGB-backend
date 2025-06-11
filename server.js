ee60b7f5fb4cae9a23daa37dadeb49547e0511e6
Koffi Eric Rainier <erickoffi29@gmail.com>
2025-06-11 01:22:44 +0000

    add partenaire render

--- server.js
+++ server.js
@@ -27,7 +27,8 @@ const app = express();
 const corsOptions = {
   origin: [
     process.env.FRONTEND_URL || 'http://localhost:3000',
-    process.env.ADMIN_URL || 'http://localhost:3001'
+    process.env.ADMIN_URL || 'http://localhost:3001',
+    'https://partenairemagb-frontend.onrender.com'
   ],
   credentials: true,
   optionsSuccessStatus: 200
